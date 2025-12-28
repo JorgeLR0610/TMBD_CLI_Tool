80fc3f818e2c888267a602cbd15ef3a901955887
JorgeLR0610 <jorgelr061005@gmail.com>
2025-12-27 21:29:29 -0600

    Refactor movie fetching functionality to support dynamic movie types

--- core/tmdb_client.py
+++ core/tmdb_client.py
@@ -1,11 +1,12 @@
 import httpx
 import os
 from dotenv import load_dotenv
+from fastapi import HTTPException
 
 load_dotenv()
 
-async def fetch_now_playing():
-    base: str = "https://api.themoviedb.org/3/movie/now_playing"
+async def fetch_data_type(type: str):
+    base: str = f"https://api.themoviedb.org/3/movie/{type}"
     
     headers = {
         "accept": "application/json",
@@ -17,9 +18,34 @@ async def fetch_now_playing():
         "page": 1
     }
     
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(base, headers=headers, params=params, timeout=30)
-        resp.raise_for_status()
-    
-    data = resp.json()
+    try:
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(base, headers=headers, params=params, timeout=30)
+            resp.raise_for_status()
+            data = resp.json()
+        
+    except httpx.HTTPStatusError as e: # Case 1: The API returned an error code, like 404, 505 or whatever
+        print(f'API error: {e.response.status_code}')
+        
+        raise HTTPException( # Throw error onto the main program
+            status_code=e.response.status_code,
+            detail=f"Error fetching movies: {e.response.text}"
+        )
+        
+    except httpx.RequestError as e: # Case 2: Connection error (Timeout, dns, no internet)
+        print(f"Conection error: {e}")
+        
+        raise HTTPException(# Throw error onto the main program
+            status_code=503, # Service unavailable
+            detail="It wasn't possible to connect with the external movie service"
+        )
+        
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        
+        raise HTTPException(
+            status_code=500,
+            detail="Internal server error"
+        ) 
+        
     return data
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+import asyncio
 import core.tmdb_client 
+import argparse
 
-app =  FastAPI()
+parser = argparse.ArgumentParser()
 
-@app.get("/now_playing")
-async def now_playing():
-    return await core.tmdb_client.fetch_now_playing()
+parser.add_argument('-t', "--type", type=str, help="Type the kind of movies you want to display info about, be it 'playing', 'popular', 'top' or 'upcoming'", required=True, choices=['playing', 'top', 'popular', 'upcoming']) #String by default actually
+args = parser.parse_args()
+
+async def fetch_data(type: str):
+    return await core.tmdb_client.fetch_data_type(type)
+
+if args.type == 'playing':
+    type: str = 'now_playing'
+    print(asyncio.run(fetch_data(type)))
+elif args.type == 'popular':
+    print(asyncio.run(fetch_data(args.type)))
+elif args.type == 'top':
+    type: str = 'top_rated'
+    print(asyncio.run(fetch_data(type)))
+elif args.type == 'upcoming':
+    print(asyncio.run(fetch_data(args.type)))
+else:
+    print('Type a valid option')
